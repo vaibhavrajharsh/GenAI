2ebd1c0cac7144719f97ff1a8f5105978d6af656
Vaibhav Raj Harsh <156331614+vaibhavrajharsh@users.noreply.github.com>
2026-06-24 16:09:59 +0530

    Setup : MOngoDB Database using mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-require("dotenv").config()
-const app = require('./src/app');
+require("dotenv").config();
+const app = require("./src/app");
+const ConnectDB = require("./src/config/db");
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+ConnectDB();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+async function ConnectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DB");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = ConnectDB;
