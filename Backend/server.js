2aae22ca3e8e2cf3bdcbba8388d1c014eda6078d
Vaibhav Raj Harsh <156331614+vaibhavrajharsh@users.noreply.github.com>
2026-07-13 23:42:36 +0530

    feat: add interview report generation with Zod validation and Google GenAI integration

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app");
 const ConnectDB = require("./src/config/db");
+const invokeGeminiApi = require("./src/services/ai.service");
 
 ConnectDB();
+invokeGeminiApi();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
